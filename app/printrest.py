9d1c9d0fe109da737bf64623c09364c73dbd1169
tbjep <tbjepsen1@gmail.com>
2026-02-12 12:04:05 +0100

    Change cdn to cdnjs.com
    getmdl.io has stopped working

--- app/printrest.py
+++ app/printrest.py
@@ -69,8 +69,8 @@ print_upload_form = """
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>POP Print</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.min.js"></script>
   <style>
     body {
       font-family: 'Roboto', sans-serif;
